3f76cb09a17d0f575a0cdf385f503b1b537eb58c
masum <promotionbd321@gmail.com>
2025-02-23 12:23:40 +0600

    controller resarch

--- lib/app/bindings/dependency_injection.dart
+++ lib/app/bindings/dependency_injection.dart
@@ -3,34 +3,30 @@ import 'package:get/get.dart';
 class DependencyInjection extends Bindings {
   @override
   void dependencies() {
-    // ✅✅✅✅Eager initialization✅✅✅✅ - instance তৈরি হবে এখনই
-    // Get.put(() => AuthController(), permanent: true);
+    // ✅Eager initialization✅ - instance তৈরি হবে এখনই
+    // Get.put(() => HomeController(), permanent: true);
 
-    // ✅✅✅✅Lazy initialization✅✅✅✅ - instance তৈরি হবে যখন প্রয়োজন হবে
-    // Get.lazyPut(() => AuthController(), fenix: true);
+    // ✅Lazy initialization✅ - instance তৈরি হবে যখন প্রয়োজন হবে
+    // Get.lazyPut(() => HomeController(), fenix: true);
   }
 }
-
 /*
-
-//✅✅✅✅ Why i Use Get.lazyPut??✅✅✅✅
+//✅ Why i Use Get.lazyPut??✅
 
 আপনি Get.lazyPut() ব্যবহার করেছেন, যা নিশ্চিত করে যে controller গুলো তখন instantiate হবে যখন সত্যিই
  দরকার হবে।এতে memory-র উপর অতিরিক্ত চাপ কমে যায়।
  Fenix: true থাকায় যদি কোনো controller dispose হয়েযায়, পুনরায় প্রয়োজনে তা reinitialize হবে।
 
-
-// ✅✅✅✅When I use Get.lazyput?✅✅✅✅
-
+// ✅When I use Get.lazyput?✅
 যদি আপনি চান যে, controller বা dependency শুধুমাত্র যখন দরকার তখন instantiate হোক,
  তাহলে Get.lazyPut ব্যবহার করা ভালো। এতে করে unnecessary memory usage কমে এবং performance উন্নত হয়।
 
- //✅✅✅✅ Why use Permanent true ?✅✅✅✅
+ //✅ Why use Permanent true ?✅
  permanent: true:
 এই option টি ব্যবহার করলে AuthController instance টি সারা App lifetime ধরে memory তে থাকবে, অর্থাৎ, এটি dispose বা বন্ধ হয়ে যাবে না যতক্ষন না App সম্পূর্ণভাবে বন্ধ করা হয়।
 
 
-✅✅✅✅ GetX-এ মূলত দু’প্রকার initialization পদ্ধতি ব্যবহার করা হয়:✅✅✅✅
+✅ GetX-এ মূলত দু’প্রকার initialization পদ্ধতি ব্যবহার করা হয়:✅
 
 ✅✅✅✅Eager Initialization:
 Instance তৈরি হয় binding execution এর সময়।
--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -3,3 +3,14 @@ import 'package:get/get.dart';
 class HomeController extends GetxController{
 
 }
+
+
+/*
+onInit(): Controller initialize হওয়ার সময় কল হয়। এখানে আপনার প্রাথমিক setup, যেমন API call বা initial data load করা যায়।
+onReady(): যখন Controller সম্পূর্ণভাবে UI এর সাথে যুক্ত হয়ে যায় তখন এটি কল হয়।
+onClose(): Controller dispose করার সময় এই মেথডটি কল হয়, যাতে আপনি resource বা memory release করতে পারেন।
+
+
+GetxController এর মাধ্যমে Rx (reactive) variables ব্যবহার করে UI কে সহজে reactive করা যায়।
+উদাহরণস্বরূপ, যদি আপনি একটি count variable (.obs) ব্যবহার করেন, তবে Obx widget ব্যবহার করে সেই variable এর পরিবর্তনে UI স্বয়ংক্রিয়ভাবে আপডেট হবে।
+ */
